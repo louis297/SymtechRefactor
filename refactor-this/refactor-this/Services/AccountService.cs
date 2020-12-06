7b32545fca3cf9bb873cc43ed944138241cea6d6
xl297 <xl297@georgetown.edu>
2020-12-07 04:20:04 +1300

    add sample doc-comment

--- refactor-this/refactor-this/Services/AccountService.cs
+++ refactor-this/refactor-this/Services/AccountService.cs
@@ -7,6 +7,9 @@ using System.Web;
 
 namespace refactor_this.Services
 {
+    /// <summary>
+    /// The service layer for <c>AccountController</c>
+    /// </summary>
     public class AccountService: IAccountService
     {
         private RefactorDbContext _dbcontext;
@@ -15,14 +18,20 @@ namespace refactor_this.Services
         {
             _dbcontext = new RefactorDbContext();
         }
-
+        /// <summary>
+        /// Insert a new account into database
+        /// </summary>
+        /// <param name="account">account should be a complete <c>Account</c> data record, except its Id being generated</param>
         public void AddAccount(Account account)
         {
             account.Id = Guid.NewGuid();
             _dbcontext.Accounts.Add(account);
             _dbcontext.SaveChanges();
         }
-
+        /// <summary>
+        /// Delete a row from database, should throw <c>DataNotExistException</c> if account not found
+        /// </summary>
+        /// <param name="ID">ID should be the guid of the account</param>
         public void DeleteAccount(Guid ID)
         {
             var account = GetAccountByID(ID);
@@ -34,6 +43,11 @@ namespace refactor_this.Services
                 .Remove(account);
             _dbcontext.SaveChanges();
         }
+        /// <summary>
+        /// Select an account through its ID
+        /// </summary>
+        /// <param name="ID">ID should be the guid of the account</param>
+        /// <returns>The account found or null</returns>
         public Account GetAccountByID(Guid ID)
         {
             try
@@ -47,12 +61,19 @@ namespace refactor_this.Services
             }
 
         }
-
+        /// <summary>
+        /// Select all accounts from database
+        /// </summary>
+        /// <returns>All accounts from database in <c>IEnumerable</c> format</returns>
         public IEnumerable<Account> GetAccounts()
         {
             return _dbcontext.Accounts;
         }
-
+        /// <summary>
+        /// Update an account from database, should throw <c>DataNotExistException</c> if account not found
+        /// </summary>
+        /// <param name="ID">The ID of the account being modified</param>
+        /// <param name="newAccount">newAccount should be a complete <c>Account</c> data record</param>
         public void ModifyAccount(Guid ID, Account newAccount)
         {
             var account = GetAccountByID(ID);
